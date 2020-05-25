2d593161f12bca3c8cdf0ce5c1364d7eaa86b147
Venkata Buddhiraju <nagarakesh4@gmail.com>
2020-05-25 12:13:24 -0700

    start showing transaction in rows and columns

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyHomePage extends StatelessWidget {
   final List<Transaction> transactions = [
     Transaction(
       id: 't0',
-      title: 'Croissants',
+      title: 'Croissant',
       amount: 12.99,
       dateTime: DateTime.now(),
     ),
@@ -55,7 +55,25 @@ class MyHomePage extends StatelessWidget {
           Column(
             children: transactions.map((tx) {
               return Card(
-                child: Text(tx.title),
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Text(
+                        tx.amount.toString(),
+                      ),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Text(
+                          tx.title,
+                        ),
+                        Text(
+                          tx.dateTime.toString(),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               );
             }).toList(),
           )
