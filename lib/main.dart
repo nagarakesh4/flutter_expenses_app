64326f59185e57c2660e5e8d99404eead7d02019
Venkata Buddhiraju <nagarakesh4@gmail.com>
2020-05-27 05:41:18 -0700

    update appbar to include icon widget, add floating actionbutton

--- lib/main.dart
+++ lib/main.dart
@@ -28,9 +28,15 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
       ),
       body: SingleChildScrollView(
-              child: Column(
+        child: Column(
           // mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             // chart
@@ -46,6 +52,12 @@ class MyHomePage extends StatelessWidget {
             UserTransactions()
           ],
         ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+        backgroundColor: Colors.blue,
       ),
     );
   }
--- lib/widgets/list_transactions.dart
+++ lib/widgets/list_transactions.dart
@@ -12,7 +12,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 1000,
       child: ListView.builder(
         itemBuilder: (ctx, index) {
           return Card(
