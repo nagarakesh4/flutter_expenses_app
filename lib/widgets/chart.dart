0c47e9778d8f3e3c4cfc1a34a56b063c35253a2e
Venkata Buddhiraju <nagarakesh4@gmail.com>
2020-06-03 08:13:21 -0700

    101. add chart bar widget to start showing charts

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -2,6 +2,7 @@ import 'package:intl/intl.dart';
 
 import '../models/transaction.dart';
 import 'package:flutter/material.dart';
+import './chart_bar.dart';
 
 class Chart extends StatelessWidget {
   //will be sent by external class
@@ -32,9 +33,15 @@ class Chart extends StatelessWidget {
       print('today day:: ${DateFormat.E().format(weekDay)}');
 
       return {
-        'day': DateFormat.E().format(weekDay),
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalSum,
       };
+    });
+  }
+
+  double get totalSpending{
+    return groupedTransactionValues.fold(0.0, (sum, item){
+      return sum + item['amount'];
     });
   }
 
@@ -45,7 +52,14 @@ class Chart extends StatelessWidget {
       elevation: 8,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: <Widget>[],
+        children: groupedTransactionValues.map((data) {
+          //display chartbar
+          return ChartBar(
+            label: data['day'],
+            spendingAmount: data['amount'],
+            spendingPercentTotal: totalSpending == 0.0 ? 0.0 : (data['amount'] as double)/totalSpending,
+          );
+        }).toList(),
       ),
     );
   }
---
+++ lib/widgets/chart_bar.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  //add variables to be used in this widget
+  final String label;
+  final double spendingAmount;
+  final double spendingPercentTotal;
+
+  ChartBar({this.label, this.spendingAmount, this.spendingPercentTotal});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        //amount
+        Text('\$${spendingAmount.toStringAsFixed(0)}'),
+        //empty space
+        SizedBox(
+          height: 4,
+        ),
+        //bar
+        Container(
+          height: 60,
+          width: 10,
+          //child is background fraction color
+          child: Stack(
+            children: <Widget>[
+              //bottom container
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey, width: 1.0),
+                  color: Color.fromRGBO(220, 220, 220, 1),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              //overlay container
+              FractionallySizedBox(
+                heightFactor: spendingPercentTotal,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        //empty space
+        SizedBox(
+          height: 4,
+        ),
+        //label
+        Text(
+          label,
+        ),
+      ],
+    );
+  }
+}
